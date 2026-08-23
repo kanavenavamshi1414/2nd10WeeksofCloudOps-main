6f11ce3100d4f6685f88766d6001860c0cdf55fc
kanavenavamshi1414 <kanavenavamshi1414@gmail.com>
2026-08-23 20:20:59 +0530

    Update main.tf

--- ROOT-terraform/envs/main.tf
+++ ROOT-terraform/envs/main.tf
@@ -6,15 +6,18 @@ terraform {
     }
   }
 
- 
-
   required_version = ">= 1.6.3"
+
+  backend "s3" {
+    bucket = "naresh-terraform-state-20260823"
+    key    = "envs/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
   region = var.region
 }
-
 
 module "networking" {
   source = "../modules/networking"
@@ -25,7 +28,7 @@ module "networking" {
   availability_zones      = var.availability_zones
   jumphost_ami_id         = var.jumphost_ami_id
   jumphost_instance_type  = var.jumphost_instance_type
-  # jumphost_key_name       = var.jumphost_key_name
+  # jumphost_key_name     = var.jumphost_key_name
   jumphost_name           = var.jumphost_name
   install_tools_user_data = templatefile("${path.module}/install-tools.sh", {})
 }
@@ -33,15 +36,15 @@ module "networking" {
 module "eks" {
   source = "../modules/eks"
 
-  cluster_name         = var.cluster_name
-  cluster_version      = var.cluster_version
-  private_subnet_ids   = module.networking.private_subnet_ids
-  public_subnet_id     = module.networking.public_subnet_ids[0]
-  security_group_id    = module.networking.security_group_id
-  node_instance_types  = var.node_instance_types
-  node_desired_size    = var.node_desired_size
-  node_max_size        = var.node_max_size
-  node_min_size        = var.node_min_size
+  cluster_name        = var.cluster_name
+  cluster_version     = var.cluster_version
+  private_subnet_ids  = module.networking.private_subnet_ids
+  public_subnet_id    = module.networking.public_subnet_ids[0]
+  security_group_id   = module.networking.security_group_id
+  node_instance_types = var.node_instance_types
+  node_desired_size   = var.node_desired_size
+  node_max_size       = var.node_max_size
+  node_min_size       = var.node_min_size
 }
 
 module "database" {
